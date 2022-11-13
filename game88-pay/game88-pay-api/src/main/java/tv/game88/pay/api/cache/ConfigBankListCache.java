5241512065fec19cf13306ca3b945868642049b2
mengjun <mengjun8877@gmail.com>
2022-11-14 01:26:35 +0800

    fix

--- game88-pay/game88-pay-api/src/main/java/tv/game88/pay/api/cache/ConfigBankListCache.java
+++ game88-pay/game88-pay-api/src/main/java/tv/game88/pay/api/cache/ConfigBankListCache.java
@@ -1,8 +1,11 @@
 package tv.game88.pay.api.cache;
 
 import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import tv.game88.common.utils.JsonUtil;
 import tv.game88.common.utils.RedisUtils;
 import tv.game88.core.config.constants.Constants;
@@ -11,6 +14,7 @@ import tv.game88.pay.api.entity.ConfigBankList;
 import tv.game88.pay.api.mapper.ConfigBankListMapper;
 
 import javax.annotation.Resource;
+import java.time.Duration;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -21,22 +25,44 @@ import java.util.stream.Collectors;
 public class ConfigBankListCache {
     private static final String CONFIG_BANKLIST_CACHE = Constants.CONFIG_PREX + "bankList";
 
+    private static final Cache<String, List<RspConfigBankList>> LOCAL_CACHE = Caffeine.newBuilder()
+            // 设置最后一次写入或访问后经过固定时间过期
+            .expireAfterWrite( Duration.ofSeconds( 10 ) )
+            // 初始的缓存空间大小
+            .initialCapacity( 1 )
+            // 缓存的最大条数
+            .maximumSize( 1 )
+            // 构建
+            .build();
+
     @Resource
     private ConfigBankListMapper configBankListMapper;
     @Resource
     private RedisUtils           redisUtils;
 
     public List<RspConfigBankList> getEffectList() {
-        exists();
-        return redisUtils.hGetAll( CONFIG_BANKLIST_CACHE ).values().stream()
-                .map( obj -> JsonUtil.json2Object( obj.toString(), RspConfigBankList.class ) )
-                .sorted( Comparator.comparing( o -> o.getSort() ) ).collect( Collectors.toList() );
+        List<RspConfigBankList> localCacheIfPresent = LOCAL_CACHE.getIfPresent( CONFIG_BANKLIST_CACHE );
+        if ( CollectionUtils.isEmpty( localCacheIfPresent ) ) {
+            exists();
+            List<RspConfigBankList> rspConfigBankLists = redisUtils
+                    .hGetAll( CONFIG_BANKLIST_CACHE )
+                    .values()
+                    .stream()
+                    .map( obj -> JsonUtil.json2Object( obj.toString(), RspConfigBankList.class ) )
+                    .sorted( Comparator.comparing( o -> o.getSort() ) )
+                    .collect( Collectors.toList() );
+            LOCAL_CACHE.put( CONFIG_BANKLIST_CACHE, rspConfigBankLists );
+            return rspConfigBankLists;
+        }
+        return localCacheIfPresent;
     }
 
     public void exists() {
         if ( !redisUtils.exists( CONFIG_BANKLIST_CACHE ) ) {
-            List<ConfigBankList> bankListList = new QueryChainWrapper<>( configBankListMapper ).eq( "effect", 1 )
-                    .select( "id", "bank_name", "bank_icon", "sort" ).list();
+            List<ConfigBankList> bankListList = new QueryChainWrapper<>( configBankListMapper )
+                    .eq( "effect", 1 )
+                    .select( "id", "bank_name", "bank_icon", "sort" )
+                    .list();
 
             Map<String, String> map = new HashMap<>();
             for ( ConfigBankList configBankList : bankListList ) {
