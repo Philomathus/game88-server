7b8965881c234b370bc9177c337b79d6b1ad2665
rajesh <rkyadavsoft@gmail.com>
2022-11-17 15:13:19 +0800

    fixed icon display

--- game88-lottery/game88-lottery-api/src/main/java/tv/game88/lottery/api/service/impl/LotteryInfoServiceImpl.java
+++ game88-lottery/game88-lottery-api/src/main/java/tv/game88/lottery/api/service/impl/LotteryInfoServiceImpl.java
@@ -2,6 +2,9 @@ package tv.game88.lottery.api.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import tv.game88.common.utils.StringUtils;
+import tv.game88.core.config.cache.ConfigDomainCacheUtil;
 import tv.game88.lottery.api.entity.LotteryInfo;
 import tv.game88.lottery.api.mapper.LotteryInfoMapper;
 import tv.game88.lottery.api.service.LotteryInfoService;
@@ -24,6 +27,16 @@ public class LotteryInfoServiceImpl extends ServiceImpl<LotteryInfoMapper, Lotte
      */
     @Override
     public List<LotteryInfo> selectLotteryInfoList( LotteryInfo lotteryInfo ) {
-        return this.baseMapper.selectLotteryInfoList( lotteryInfo );
+        List<LotteryInfo> lotteryInfos = this.baseMapper.selectLotteryInfoList( lotteryInfo );
+
+        String                domainValue      = ConfigDomainCacheUtil.me.getDomainOssValue();
+        if ( !CollectionUtils.isEmpty( lotteryInfos ) ) {
+            for ( LotteryInfo info : lotteryInfos ) {
+                if ( StringUtils.isNotBlank( info.getIcon() ) && !info.getIcon().startsWith( "http" ) ) {
+                    info.setIcon( domainValue + info.getIcon() );
+                }
+            }
+        }
+        return lotteryInfos;
     }
 }
