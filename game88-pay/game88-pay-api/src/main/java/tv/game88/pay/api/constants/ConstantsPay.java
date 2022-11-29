3c4d74aa5f9f53a5eeab067724269e25822305d1
mengjun <mengjun8877@gmail.com>
2022-11-30 01:04:13 +0800

    fix

--- game88-pay/game88-pay-api/src/main/java/tv/game88/pay/api/constants/ConstantsPay.java
+++ game88-pay/game88-pay-api/src/main/java/tv/game88/pay/api/constants/ConstantsPay.java
@@ -8,5 +8,6 @@ package tv.game88.pay.api.constants;
 public abstract class ConstantsPay {
     // LB支付
     public static final String LB_PAY      = "LBPay";
-    public static final String    HUOJIAN_PAY = "huoJianPay";
+    public static final String HUOJIAN_PAY = "huoJianPay";
+    public static final String TONGDA_PAY  = "tongDaPay";
 }
