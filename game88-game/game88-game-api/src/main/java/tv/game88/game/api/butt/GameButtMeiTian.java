e2a0bdf2ba363efdd94906ca1218f7eb18da6ea3
mengjun <mengjun8877@gmail.com>
2022-12-09 19:49:48 +0800

    fix

--- game88-game/game88-game-api/src/main/java/tv/game88/game/api/butt/GameButtMeiTian.java
+++ game88-game/game88-game-api/src/main/java/tv/game88/game/api/butt/GameButtMeiTian.java
@@ -104,7 +104,7 @@ public class GameButtMeiTian extends AbstractGameButt {
             }
         }
         if ( StringUtils.isBlank( reqJoinGame.getGameUrl() ) ) {
-            log.error( "ICG获取游戏链接失败:{}; userId:{}", JsonUtil.object2Json( resultMap ), reqJoinGame.getGameMemberId() );
+            log.error( "meitian 获取游戏链接失败:{}; userId:{}", JsonUtil.object2Json( resultMap ), reqJoinGame.getGameMemberId() );
             throw new BusinessException( "获取游戏链接失败" );
         }
     }
