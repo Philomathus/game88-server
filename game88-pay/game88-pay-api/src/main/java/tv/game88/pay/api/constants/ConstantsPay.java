15370a6f996903716b4a87fecf1bbb8195c1b753
Goya <agoya@test.com>
2022-12-21 18:01:00 +0800

    Added JinDunPayProcessor

--- game88-pay/game88-pay-api/src/main/java/tv/game88/pay/api/constants/ConstantsPay.java
+++ game88-pay/game88-pay-api/src/main/java/tv/game88/pay/api/constants/ConstantsPay.java
@@ -39,4 +39,5 @@ public abstract class ConstantsPay {
     public static final String BAIFU_PAY      = "baiFuPay";
     public static final String NAN_BEI_PAY      = "nanBeiPay";
     public static final String LAN_BO_PAY      = "lanBoPay";
+    public static final String JIN_DUN_PAY      = "jinDunPay";
 }
