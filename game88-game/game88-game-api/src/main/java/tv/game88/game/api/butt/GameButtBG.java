23c50c7957f59a840ef904a0d8684628e33e56af
mengjun <mengjun8877@gmail.com>
2022-12-22 16:50:40 +0800

    new game

---
+++ game88-game/game88-game-api/src/main/java/tv/game88/game/api/butt/GameButtBG.java
@@ -0,0 +1,48 @@
+package tv.game88.game.api.butt;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Repository;
+import tv.game88.game.api.base.AbstractGameButt;
+import tv.game88.game.api.constants.ConstantsGame;
+import tv.game88.game.api.dto.ReqJoinGame;
+
+import java.math.BigDecimal;
+
+@Log4j2
+@Repository( value = ConstantsGame.BG + "GameProcessor" )
+public class GameButtBG extends AbstractGameButt {
+    @Override
+    public void getToken( ReqJoinGame reqJoinGame ) {
+
+    }
+
+    @Override
+    public void createAccount( ReqJoinGame reqJoinGame ) {
+
+    }
+
+    @Override
+    public void getJoinGameUrl( ReqJoinGame reqJoinGame ) {
+
+    }
+
+    @Override
+    public void transferMoney( ReqJoinGame reqJoinGame ) {
+
+    }
+
+    @Override
+    public void withdrawal( ReqJoinGame reqJoinGame ) {
+
+    }
+
+    @Override
+    public BigDecimal queryBalance( ReqJoinGame reqJoinGame ) {
+        return null;
+    }
+
+    @Override
+    public boolean queryTransfer( ReqJoinGame reqJoinGame ) {
+        return false;
+    }
+}
