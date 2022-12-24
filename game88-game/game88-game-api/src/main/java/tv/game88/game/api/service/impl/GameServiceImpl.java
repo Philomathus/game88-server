c2beb6eddfcd96a2ce69d6a411baabdbf517931c
mengjun <mengjun8877@gmail.com>
2022-12-24 21:08:00 +0800

    BG棋牌对接完成

--- game88-game/game88-game-api/src/main/java/tv/game88/game/api/service/impl/GameServiceImpl.java
+++ game88-game/game88-game-api/src/main/java/tv/game88/game/api/service/impl/GameServiceImpl.java
@@ -322,7 +322,7 @@ public class GameServiceImpl implements GameService {
 
     private String getGameOrderId( String gameMemberId, String agent, GamePlatform gamePlatform ) {
         return switch ( gamePlatform.getGameCategory() ) {
-            case AG, BBIN -> this.getGameAtomicId( gamePlatform.getId() );
+            case AG, BBIN, BG -> this.getGameAtomicId( gamePlatform.getId() );
             case MEITIAN -> agent
                     .concat( LocalDateTimeUtils.format( LocalDateTime.now(), LocalDateTimeUtils.YYYYMMDDHHMMSSSSS_FORMATTER ) )
                     .concat( gameMemberId.replaceAll( "_", "" ) );
