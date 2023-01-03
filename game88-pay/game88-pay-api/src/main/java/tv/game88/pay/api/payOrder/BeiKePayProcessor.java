03d1bc193d26aad01b3ff7385baae86bc600e650
Goya <agoya@test.com>
2023-01-03 16:59:03 +0800

    Updated BeiKePayProcessor

--- game88-pay/game88-pay-api/src/main/java/tv/game88/pay/api/payOrder/BeiKePayProcessor.java
+++ game88-pay/game88-pay-api/src/main/java/tv/game88/pay/api/payOrder/BeiKePayProcessor.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import org.springframework.util.CollectionUtils;
 import tv.game88.common.utils.AESCoder;
 import tv.game88.common.utils.JsonUtil;
-import tv.game88.common.utils.LocalDateTimeUtils;
 import tv.game88.pay.api.base.AbstractPay;
 import tv.game88.pay.api.constants.ConstantsPay;
 import tv.game88.pay.api.dto.ReqPayRecharge;
@@ -17,10 +16,10 @@ import tv.game88.pay.api.entity.PayPlatform;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
+import java.util.UUID;
 
 @Repository(value = ConstantsPay.BEIKE_PAY + "Processor")
 @Log4j2
@@ -31,68 +30,64 @@ public class BeiKePayProcessor extends AbstractPay {
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public String orderPay(PayChannel payChannel, PayPlatform payPlatform, ReqPayRecharge reqPayRecharge) {
         Map<String, Object> params = new TreeMap<>();
-        params.put("mchId", payPlatform.getMerId());
-        params.put("productId", Integer.parseInt(payChannel.getChannelCode()));
-        params.put("mchOrderNo", reqPayRecharge.getOrderNo());
-        params.put("amount", reqPayRecharge
-                .getMoney()
-                .multiply(BigDecimal.valueOf(100))
-                .setScale(0, RoundingMode.HALF_UP)
-                .toString());
-        params.put("currency", "cny");
-        params.put("notifyUrl", configEnvCacheUtil.getConf("payCallbackUrl") + payPlatform.getCode());
-        params.put("subject", "subject");
-        params.put("body", "body");
-        params.put("reqTime", LocalDateTimeUtils.format(LocalDateTime.now(), LocalDateTimeUtils.YYYYMMDDHHMMSS_FORMATTER));
-        params.put("version", "1.0");
+        params.put("account_id", payPlatform.getMerId());
+        params.put("thoroughfare", payChannel.getChannelCode());
+        params.put("out_trade_no", reqPayRecharge.getOrderNo());
+        params.put("amount", reqPayRecharge.getMoney().setScale(2, RoundingMode.HALF_UP));
+        params.put("callback_url", configEnvCacheUtil.getConf("payCallbackUrl") + payPlatform.getCode());
+        params.put("nonce_str", UUID.randomUUID().toString().replace("-", ""));
+        params.put("content_type", "json_new");
+        params.put("robin", "1");
 
         String tempStr = this.assemblyUrl(params) + "&key=" + AESCoder.decrypt(payPlatform.getSignMd5());
         log.warn("Order: {}", tempStr);
-        String sign = DigestUtils.md5Hex(tempStr).toUpperCase();
+        String sign = DigestUtils.md5Hex(tempStr).toLowerCase();
         params.put("sign", sign);
 
-        Map<String, Object> resultMap = this.sendPostMap(payPlatform.getPayUrl(), packageForm(params), reqPayRecharge);
+        Map<String, Object> resultMap = this.sendPostMap(payPlatform.getPayUrl(), packageJson(params), reqPayRecharge);
 
         log.warn(payPlatform.getName()
                         + "下单结果:{},支付通道:{},订单号:{}", JsonUtil.object2Json(resultMap), payChannel.getChannelCode(),
                 reqPayRecharge.getOrderNo());
 
         if (!CollectionUtils.isEmpty(resultMap)) {
-            String code = resultMap.getOrDefault("retCode", "").toString();
-            int status = Integer.parseInt(resultMap.getOrDefault("orderStatus", -1).toString());
-            if ("0".equals(code) && status == 2 || status == 3) {
-                return resultMap.get("payUrl").toString();
+            String code = resultMap.getOrDefault("code", "").toString();
+            Map<String, Object> dataMap = (Map<String, Object>) resultMap.get("data");
+            if ("200".equals(code) && !CollectionUtils.isEmpty(dataMap)) {
+                return resultMap.get("qr_str").toString();
             } else {
-                reqPayRecharge.setFailReason(resultMap.getOrDefault("retMsg", "").toString());
+                reqPayRecharge.setFailReason(resultMap.getOrDefault("msg", "").toString());
             }
         }
         return null;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public boolean queryPay(MemberRechargeOnline memberRechargeOnline, PayPlatform payPlatform, PayChannel payChannel) {
         SortedMap<String, Object> params = new TreeMap<>();
-        params.put("mchId", payPlatform.getMerId());
-        params.put("mchOrderNo", memberRechargeOnline.getOrderNo());
-        params.put("reqTime", LocalDateTimeUtils.format(LocalDateTime.now(), LocalDateTimeUtils.YYYYMMDDHHMMSS_FORMATTER));
-        params.put("version", "1.0");
+        params.put("acc_id", payPlatform.getMerId());
+        params.put("out_trade_no", memberRechargeOnline.getOrderNo());
+        params.put("nonce_str", UUID.randomUUID().toString().replace("-", ""));
 
         String signStr = this.assemblyUrl(params) + "&key=" + AESCoder.decrypt(payPlatform.getSignMd5());
         log.warn("Query: {}", signStr);
-        params.put("sign", DigestUtils.md5Hex(signStr).toUpperCase());
+        params.put("sign", DigestUtils.md5Hex(signStr).toLowerCase());
 
-        Map<String, Object> resultMap = this.sendPostMap(payPlatform.getQueryUrl(), packageForm(params), null);
+        Map<String, Object> resultMap = this.sendPostMap(payPlatform.getQueryUrl(), packageJson(params), null);
 
         log.warn(payPlatform.getName()
                 + "查询结果 - orderNo:{};result:{}", memberRechargeOnline.getOrderNo(), JsonUtil.object2Json(resultMap));
 
         if (!CollectionUtils.isEmpty(resultMap)) {
-            String code = resultMap.getOrDefault("retCode", "").toString();
-            int status = Integer.parseInt(resultMap.getOrDefault("status", -1).toString());
-            if ("0".equals(code) && status == 2 || status == 3) {
-                BigDecimal amount = new BigDecimal(resultMap.getOrDefault("amount", 0).toString());
+            String code = resultMap.getOrDefault("code", "").toString();
+            Map<String, Object> dataMap = (Map<String, Object>) resultMap.get("data");
+            if ("200".equals(code) && !CollectionUtils.isEmpty(dataMap) &&
+                    "1".equals(dataMap.getOrDefault("status", "-1"))) {
+                BigDecimal amount = new BigDecimal(dataMap.getOrDefault("amount", 0).toString());
                 memberRechargeOnline.setRealMoney(amount.divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP));
                 return true;
             }
@@ -102,8 +97,8 @@ public class BeiKePayProcessor extends AbstractPay {
 
     @Override
     public String callbackPay(Map<String, Object> requestMap, String realIp) {
-        String mchOrderNo = requestMap.getOrDefault("mchOrderNo", "").toString();
-        String payOrderId = requestMap.getOrDefault("payOrderId", "").toString();
+        String mchOrderNo = requestMap.getOrDefault("out_trade_no", "").toString();
+        String payOrderId = requestMap.getOrDefault("trade_no", "").toString();
         MemberRechargeOnline memberRechargeOnline = memberRechargeOnlineMapper.selectById(mchOrderNo);
 
         if (memberRechargeOnline.getStatus() == 1) {
@@ -126,10 +121,6 @@ public class BeiKePayProcessor extends AbstractPay {
         }
 
         String sign = requestMap.remove("sign").toString();
-        requestMap.remove("appId");
-        requestMap.remove("channelOrderNo");
-        requestMap.remove("param1");
-        requestMap.remove("param2");
         // 去除空值
         requestMap.entrySet().removeIf(me -> me.getValue() == null || StringUtils.isBlank(me.getValue().toString()));
 
@@ -137,12 +128,12 @@ public class BeiKePayProcessor extends AbstractPay {
 
         String signStr = this.assemblyUrl(bodyMap) + "&key=" + AESCoder.decrypt(payPlatform.getSignMd5());
         log.warn("Callback: {}", signStr);
-        String rel = DigestUtils.md5Hex(signStr).toUpperCase();
+        String rel = DigestUtils.md5Hex(signStr).toLowerCase();
 
         log.info(payPlatform.getName() + "回调签名字符串:" + sign + "_" + rel);
         if (rel.equalsIgnoreCase(sign)) {
-            int status = Integer.parseInt(requestMap.getOrDefault("status", -1).toString());
-            if ((status == 2 || status == 3) && this.queryPay(memberRechargeOnline, payPlatform, payChannel)) {
+            String status = requestMap.getOrDefault("status", "").toString();
+            if ("success".equals(status) && this.queryPay(memberRechargeOnline, payPlatform, payChannel)) {
                 memberRechargeOnline.setUpperOrderNo(payOrderId);
                 return payService.updatePayJourStatus(memberRechargeOnline, new String[]{"success", "fail"},
                         payChannel.getName());
