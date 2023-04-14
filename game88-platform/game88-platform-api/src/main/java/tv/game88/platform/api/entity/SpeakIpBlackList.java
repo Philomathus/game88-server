536d283ee8e5a981cd600b28af02466275b67873
jake gica <jakefeiwin@gmail.com>
2023-04-14 16:03:49 +0800

    /live/video/liveVideoChat implementation.
    Run this SQL in order to show this to 8080.
    
    INSERT INTO `8800`.sys_n_menu (menu_id, menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark) VALUES (2145, '会员发言', 2217, 1, 'liveVideoChat', 'live-web/chat/index', 1, 0, 'C', '0', '0', 'admin:liveVideoChat:list', 'arrow-right-2', 'admin', '2021-01-26 18:59:14', 'gebulin', '2021-01-27 09:26:50', '会员发言菜单');
    INSERT INTO `8800`.sys_n_menu (menu_id, menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark) VALUES (2217, '直播管理', 2023, 2, 'video', null, 1, 0, 'M', '0', '0', '', 'arrow-left-2', 'gebulin', '2021-01-27 09:02:00', 'gebulin', '2021-01-27 09:07:39', '');
    INSERT INTO `8800`.sys_n_menu (menu_id, menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark) VALUES (2023, '直播管理', 0, 5, 'live', null, 1, 0, 'M', '0', '0', '', 'build', 'gebulin', '2021-01-25 15:42:44', 'gebulin', '2021-01-27 09:14:22', '');
    
    INSERT INTO `8800`.sys_role_menu (role_id, menu_id)
    VALUES (2, 2145);
    
    INSERT INTO `8800`.sys_role_menu (role_id, menu_id)
    VALUES (2, 2217);
    
    INSERT INTO `8800`.sys_role_menu (role_id, menu_id)
    VALUES (2, 2023);

---
+++ game88-platform/game88-platform-api/src/main/java/tv/game88/platform/api/entity/SpeakIpBlackList.java
@@ -0,0 +1,38 @@
+package tv.game88.platform.api.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * 【请填写功能名称】对象 speak_ip_black_list
+ *
+ * @author 77tv
+ * @date 2021-02-22
+ */
+@Data
+public class SpeakIpBlackList implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /** 会员ID */
+    private Integer id;
+    private String userId;
+    private String userIp;
+    private String msg;
+    @JsonFormat( pattern = "yyyy-MM-dd HH:mm:ss" )
+    private LocalDateTime createTime;
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("id", getId())
+            .append("createTime", getCreateTime())
+            .append("userIp", getUserIp())
+            .append("msg", getMsg())
+            .toString();
+    }
+}
