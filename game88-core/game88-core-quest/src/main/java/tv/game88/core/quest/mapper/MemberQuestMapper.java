82573a7370e28fab348be74615393dcb12dcb275
jake gica <jakefeiwin@gmail.com>
2023-04-19 17:09:57 +0800

    Member quest from 77.
    #READ: Insert permissions. where $n = last index of sys_menu table. Since sys_menu id is not auto_increment
    
    INSERT INTO sys_menu (menu_id, menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, update_by, update_time, remark)
        VALUES (<$n + 1>, '会员任务', 2008, 8, 'memberQuest', 'member/memberQuest/index', 1, 0, 'C', '0', '0', 'member:memberQuest:list', 'basic-data', 'jake', 'jake', null, '会员任务菜单');
    
    INSERT INTO `8800`.sys_role_menu (role_id, menu_id)
    VALUES (2, <$n + 1>);
    
    INSERT INTO `8800`.sys_menu (menu_id,  menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
        VALUES (<$n + 2>, '查询会员任务', 1389, 1, '#', '', 1, 0, 'F', '0', '0', 'member:memberQuest:query', '#', 'jake', '2021-08-04 20:52:20', '', null, '');
    
    INSERT INTO `8800`.sys_role_menu (role_id, menu_id)
    VALUES (2, <$n + 2>);
    
    INSERT INTO `8800`.sys_menu (menu_id, menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
        VALUES (<$n + 3>, '新增会员任务', 1389, 2, '#', '', 1, 0, 'F', '0', '0', 'member:memberQuest:add', '#', 'jake', '2021-08-04 20:52:20', '', null, '');
    
    INSERT INTO `8800`.sys_role_menu (role_id, menu_id)
    VALUES (2, <$n + 3>);
    
    INSERT INTO `8800`.sys_menu (menu_id, menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
        VALUES (<$n + 4>, '删除会员任务', 1389, 4, '#', '', 1, 0, 'F', '0', '0', 'member:memberQuest:remove', '#', 'jake', '2021-08-04 20:52:20', '', null, '');
    
    INSERT INTO `8800`.sys_role_menu (role_id, menu_id)
    VALUES (2, <$n + 4>);
    
    INSERT INTO `8800`.sys_menu (menu_id, menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
        VALUES (<$n + 5>, '导出会员任务', 1389, 5, '#', '', 1, 0, 'F', '0', '0', 'member:memberQuest:export', '#', 'jake', '2021-08-04 20:52:20', '', null, '');
    
    INSERT INTO `8800`.sys_role_menu (role_id, menu_id)
    VALUES (2, <$n + 5>);
    
    INSERT INTO `8800`.sys_menu (menu_id, menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
        VALUES (<$n + 6>, '导出会员任务', 1389, 6, '#', '', 1, 0, 'F', '0', '0', 'member:memberQuest:edit', '#', 'jake', '2021-08-04 20:52:20', '', null, '');
    
    INSERT INTO `8800`.sys_role_menu (role_id, menu_id)
    VALUES (2, <$n + 6>);

--- game88-core/game88-core-quest/src/main/java/tv/game88/core/quest/mapper/MemberQuestMapper.java
+++ game88-core/game88-core-quest/src/main/java/tv/game88/core/quest/mapper/MemberQuestMapper.java
@@ -6,10 +6,57 @@ import tv.game88.core.quest.entity.MemberQuest;
 import java.util.List;
 
 public interface MemberQuestMapper extends BaseMapper<MemberQuest> {
+
+    /**
+     * 查询【请填写功能名称】
+     *
+     * @param id 【请填写功能名称】ID
+     * @return 【请填写功能名称】
+     */
+    MemberQuest selectMemberQuestById(String id);
+
+    /**
+     * 查询【请填写功能名称】列表
+     *
+     * @param memberQuest 【请填写功能名称】
+     * @return 【请填写功能名称】集合
+     */
     List<MemberQuest> selectMemberQuestList( MemberQuest memberQuest );
 
     /**
      * 重置每日任务
      */
     void resetDayTaskStatus();
+
+    /**
+     * 新增【请填写功能名称】
+     *
+     * @param memberQuest 【请填写功能名称】
+     * @return 结果
+     */
+    int insertMemberQuest(MemberQuest memberQuest);
+
+    /**
+     * 修改【请填写功能名称】
+     *
+     * @param memberQuest 【请填写功能名称】
+     * @return 结果
+     */
+    int updateMemberQuest(MemberQuest memberQuest);
+
+    /**
+     * 删除【请填写功能名称】
+     *
+     * @param id 【请填写功能名称】ID
+     * @return 结果
+     */
+    int deleteMemberQuestById(String id);
+
+    /**
+     * 批量删除【请填写功能名称】
+     *
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+    int deleteMemberQuestByIds(String[] ids );
 }
---
+++ game88-platform/game88-platform-admin/src/main/java/tv/game88/platform/admin/controller/MemberQuestController.java
@@ -0,0 +1,103 @@
+package tv.game88.platform.admin.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import tv.game88.common.base.BaseController;
+import tv.game88.common.page.PageDomain;
+import tv.game88.common.page.TableSupport;
+import tv.game88.common.utils.ExportExcelUtil;
+import tv.game88.common.vo.RspBase;
+import tv.game88.core.admin.annotation.Log;
+import tv.game88.core.admin.enums.BusinessType;
+import tv.game88.core.quest.entity.MemberQuest;
+import tv.game88.platform.api.service.MemberQuestService;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * 会员任务列表Controller
+ *
+ * @author jake from 77tv
+ * @date 2021-08-04
+ */
+@RestController
+@RequestMapping("/member/memberQuest")
+public class MemberQuestController extends BaseController {
+
+    @Resource
+    private MemberQuestService memberQuestService;
+
+    /**
+     * 查询会员任务列表
+     */
+    @PreAuthorize("@ss.hasPermi('member:memberQuest:list')")
+    @GetMapping("/list")
+    public RspBase<List<MemberQuest>> list(MemberQuest memberQuest) {
+        PageDomain pages = TableSupport.buildPageRequest();
+        startPage(pages);
+        List<MemberQuest> list = memberQuestService.selectMemberQuestList(memberQuest);
+        return getRspBasePage(list, pages);
+    }
+
+    /**
+     * 增加会员积分
+     */
+    @PreAuthorize("@ss.hasPermi('member:memberQuest:edit')")
+    @Log(title = "【会员任务】", businessType = BusinessType.UPDATE)
+    @PostMapping("/addScore")
+    public RspBase addScore(@RequestBody MemberQuest memberQuest) {
+        return toResult(memberQuestService.addMemberScore(memberQuest));
+    }
+
+    /**
+     * 导出会员任务列表
+     */
+    @PreAuthorize("@ss.hasPermi('member:memberQuest:export')")
+    @Log(title = "【会员任务】", businessType = BusinessType.EXPORT)
+    @GetMapping("/export")
+    public void export(MemberQuest memberQuest, HttpServletResponse response) {
+        List<MemberQuest> list = memberQuestService.selectMemberQuestList(memberQuest);
+        ExportExcelUtil.exportExcel(list, "【请填写功能名称】", "【请填写功能名称】表", MemberQuest.class, response);
+    }
+
+    /**
+     * 获取会员任务详细信息
+     */
+    @PreAuthorize("@ss.hasPermi('member:memberQuest:query')")
+    @GetMapping(value = "/{id}")
+    public RspBase getInfo(@PathVariable("id") String id) {
+        return RspBase.ok(memberQuestService.selectMemberQuestById(id));
+    }
+
+    /**
+     * 新增会员任务
+     */
+    @PreAuthorize("@ss.hasPermi('member:memberQuest:add')")
+    @Log(title = "【会员任务】", businessType = BusinessType.INSERT)
+    @PostMapping
+    public RspBase add(@RequestBody MemberQuest memberQuest) {
+        return toResult(memberQuestService.insertMemberQuest(memberQuest));
+    }
+
+    /**
+     * 修改会员任务
+     */
+    @PreAuthorize("@ss.hasPermi('member:memberQuest:edit')")
+    @Log(title = "【会员任务】", businessType = BusinessType.UPDATE)
+    @PutMapping
+    public RspBase edit(@RequestBody MemberQuest memberQuest) {
+        return toResult(memberQuestService.updateMemberQuest(memberQuest));
+    }
+
+    /**
+     * 删除会员任务
+     */
+    @PreAuthorize("@ss.hasPermi('member:memberQuest:remove')")
+    @Log(title = "【会员任务】", businessType = BusinessType.DELETE)
+    @DeleteMapping("/{ids}")
+    public RspBase remove(@PathVariable String[] ids) {
+        return toResult(memberQuestService.deleteMemberQuestByIds(ids));
+    }
+}
