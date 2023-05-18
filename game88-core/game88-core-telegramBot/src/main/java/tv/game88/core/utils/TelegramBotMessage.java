1dcd7b3c5b11fab3be19ff84f1d90317b2588782
mengjun <mengjun8877@gmail.com>
2023-05-19 00:52:24 +0800

    1

--- game88-core/game88-core-telegramBot/src/main/java/tv/game88/core/utils/TelegramBotMessage.java
+++ game88-core/game88-core-telegramBot/src/main/java/tv/game88/core/utils/TelegramBotMessage.java
@@ -2,6 +2,7 @@ package tv.game88.core.utils;
 
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -9,24 +10,21 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import tv.game88.core.config.cache.ConfigEnvCacheUtil;
 
-import javax.annotation.Resource;
-
 @Component
 @Log4j2
 public class TelegramBotMessage extends TelegramLongPollingBot {
-    @Resource
-    private ConfigEnvCacheUtil configEnvCacheUtil;
+    private final ConfigEnvCacheUtil configEnvCacheUtil;
+
+    @Autowired
+    public TelegramBotMessage( ConfigEnvCacheUtil configEnvCacheUtil ) {
+        super( configEnvCacheUtil.getConf( "robot_message_token" ) );
+        this.configEnvCacheUtil = configEnvCacheUtil;
+    }
 
     @Override
     public String getBotUsername() {
         // 填写username
         return configEnvCacheUtil.getConf( "bot_username_telegram", "yanQin" );
-    }
-
-    @Override
-    public String getBotToken() {
-        // 填写token
-        return configEnvCacheUtil.getConf( "robot_message_token" );
     }
 
     @Override
