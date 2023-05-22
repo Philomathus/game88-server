be7f0461a4de0dddf5b276efa42d94a9d636ff86
mengjun <mengjun8877@gmail.com>
2023-05-23 00:21:14 +0800

    1

---
+++ game88-general/game88-general-game/src/main/java/tv/game88/general/game/base/GamePullDockFactoryUtil.java
@@ -0,0 +1,22 @@
+package tv.game88.general.game.base;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+import tv.game88.core.game.type.EnumGameCategory;
+
+import javax.annotation.Resource;
+
+@Component
+public class GamePullDockFactoryUtil {
+    @Resource
+    private ApplicationContext context;
+
+    public BaseGamePull createGamePullProcessor( EnumGameCategory enumGameCategory ) {
+        return switch ( enumGameCategory ) {
+            case UPG, MG -> ( BaseGamePull ) context.getBean( EnumGameCategory.MG.getType() + "GamePullProcessor" );
+            case KAIXUAN, KAIXUAN_X, KAIYUAN, LEYOU -> ( BaseGamePull ) context.getBean(
+                    EnumGameCategory.KAIYUAN.getType() + "GamePullProcessor" );
+            default -> ( BaseGamePull ) context.getBean( enumGameCategory.getType() + "GamePullProcessor" );
+        };
+    }
+}
