4a6d2d6ab7b369deac4248e2f164694000705707
mengjun <mengjun8877@gmail.com>
2023-05-23 03:21:04 +0800

    1

---
+++ game88-general/game88-general-api/src/main/java/tv/game88/general/api/dto/ReqGameDataRecord.java
@@ -0,0 +1,10 @@
+package tv.game88.general.api.dto;
+
+import lombok.Data;
+
+@Data
+public class ReqGameDataRecord {
+    private String agent;
+    private String startTime;
+    private String endTime;
+}
--- game88-general/game88-general-api/src/main/java/tv/game88/general/api/service/GameDataRecordService.java
+++ game88-general/game88-general-api/src/main/java/tv/game88/general/api/service/GameDataRecordService.java
@@ -1,8 +1,15 @@
 package tv.game88.general.api.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import tv.game88.general.api.dto.ReqGameDataRecord;
 import tv.game88.general.api.entity.GameDataRecord;
+
+import java.util.List;
 
 public interface GameDataRecordService extends IService<GameDataRecord> {
     void cutTable( int num );
+
+    void batchInsert( List<GameDataRecord> gameDataRecords );
+
+    List<GameDataRecord> getListByReq( ReqGameDataRecord req );
 }
