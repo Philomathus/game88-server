71ab97e7e9b79a427a292c0893d3c03c9f5a383b
mengjun <mengjun8877@gmail.com>
2023-06-07 20:54:14 +0800

    1

--- game88-pay/game88-pay-api/src/main/java/tv/game88/pay/api/constants/ConstantsPayAgent.java
+++ game88-pay/game88-pay-api/src/main/java/tv/game88/pay/api/constants/ConstantsPayAgent.java
@@ -3,13 +3,14 @@ package tv.game88.pay.api.constants;
 public abstract class ConstantsPayAgent {
     public static final String PROCESSOR = "PayAgentProcessor";
 
-    public static final String LIAN_FU_BAO = "lianFuBao";
-    public static final String SHUN_WEI    = "shunWei";
-    public static final String KUBI        = "kuBi";
-    public static final String VIPPAY      = "vipPay";
-    public static final String CHONG_U     = "chongU";
-    public static final String HY          = "hy";
-    public static final String ZHAOH       = "zhaoh";
-    public static final String TO_PAY      = "toPay";
-    public static final String SANJIN_PAY  = "sanJinPay";
+    public static final String LIAN_FU_BAO    = "lianFuBao";
+    public static final String SHUN_WEI       = "shunWei";
+    public static final String KUBI           = "kuBi";
+    public static final String VIPPAY         = "vipPay";
+    public static final String CHONG_U        = "chongU";
+    public static final String HY             = "hy";
+    public static final String ZHAOH          = "zhaoh";
+    public static final String TO_PAY         = "toPay";
+    public static final String SANJIN_PAY     = "sanJinPay";
+    public static final String BB_JIUDING_PAY = "bbJiuDIngPay";
 }
