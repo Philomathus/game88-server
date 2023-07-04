cc11d3a2bc9f54df7b912102605dc7a80b2fface
mengjun <mengjun8877@gmail.com>
2023-07-05 03:23:10 +0800

    MY代付 JQ代付

--- game88-pay/game88-pay-api/src/main/java/tv/game88/pay/api/constants/ConstantsPayAgent.java
+++ game88-pay/game88-pay-api/src/main/java/tv/game88/pay/api/constants/ConstantsPayAgent.java
@@ -13,4 +13,6 @@ public abstract class ConstantsPayAgent {
     public static final String TO_PAY         = "toPay";
     public static final String SANJIN_PAY     = "sanJinPay";
     public static final String BB_JIUDING_PAY = "bbJiuDIngPay";
+    public static final String JQ_PAY         = "jqPay";
+    public static final String MY_PAY         = "myPay";
 }
---
+++ game88-pay/game88-pay-api/src/main/java/tv/game88/pay/api/type/BankCodeMyType.java
@@ -0,0 +1,53 @@
+package tv.game88.pay.api.type;
+
+import lombok.Getter;
+
+import java.util.Objects;
+
+@Getter
+public enum BankCodeMyType {
+    //
+    ICBC( 3 ),
+    CCB( 2 ),
+    ABC( 4 ),
+    PSBS( 5 ),
+    BOC( 1 ),
+    COMM( 6 ),
+    CMB( 7 ),
+    CEB( 8 ),
+    CIB( 9 ),
+    CMBC( 10 ),
+    BCCB( 11 ),
+    CITIC( 12 ),
+    GDB( 13 ),
+    SDB( 14 ),
+    SPDB( 15 ),
+    PINGANBANK( 16 ),
+    HXB( 17 ),
+    SHB( 18 ),
+    CBHB( 19 ),
+    HKBEA( 20 ),
+    NBCB( 21 ),
+    CZB( 22 ),
+    NJCB( 23 ),
+    HZCB( 24 ),
+    BJRCB( 25 ),
+    SRCB( 26 ),
+    QTBC( -1 ),
+    ;
+
+    private final Integer bankId;
+
+    BankCodeMyType( Integer bankId ) {
+        this.bankId = bankId;
+    }
+
+    public static BankCodeMyType getCodeByType( Long bankId ) {
+        for ( BankCodeMyType enumType : BankCodeMyType.values() ) {
+            if ( Objects.equals( enumType.getBankId().longValue(), bankId ) ) {
+                return enumType;
+            }
+        }
+        return null;
+    }
+}
