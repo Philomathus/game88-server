97134730c7e60d5b2ec2b439d6611d3482ceb392
mengjun <mengjun8877@gmail.com>
2023-09-25 14:45:23 +0800

    1

--- game88-general/game88-general-api/src/main/java/tv/game88/general/api/entity/AgentSecure.java
+++ game88-general/game88-general-api/src/main/java/tv/game88/general/api/entity/AgentSecure.java
@@ -6,7 +6,10 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import tv.game88.common.utils.StringUtils;
 import tv.game88.common.vo.BaseEntity;
+
+import java.util.regex.Pattern;
 
 /**
  * 域名加密管理对象 agent_secure
@@ -18,37 +21,59 @@ import tv.game88.common.vo.BaseEntity;
 public class AgentSecure extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
-    /** 代理号 */
+    /**
+     * 代理号
+     */
     @TableId( type = IdType.INPUT )
     private String id;
 
-    /** 公钥(不要显示在页面) */
+    /**
+     * 公钥(不要显示在页面)
+     */
     private String publickey;
 
-    /** 私钥(不要显示在页面) */
+    /**
+     * 私钥(不要显示在页面)
+     */
     private String privatekey;
 
-    /** 原始url */
+    /**
+     * 原始url
+     */
     private String urls;
 
-    /** 加密url */
+    /**
+     * 加密url
+     */
     private String secureUrls;
 
-    @TableField(exist = false)
+    @TableField( exist = false )
     private String remark;
+
+    public String getPublickey() {
+        if ( StringUtils.isNotBlank( publickey ) ) {
+            return Pattern.compile( "\\s*|\t|\r|\n" ).matcher( publickey ).replaceAll( "" );
+        }
+        return publickey;
+    }
+
+    public String getPrivatekey() {
+        if ( StringUtils.isNotBlank( privatekey ) ) {
+            return Pattern.compile( "\\s*|\t|\r|\n" ).matcher( privatekey ).replaceAll( "" );
+        }
+        return privatekey;
+    }
 
     @Override
     public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("id", getId())
-            .append("publickey", getPublickey())
-            .append("privatekey", getPrivatekey())
-            .append("urls", getUrls())
-            .append("secureUrls", getSecureUrls())
-            .append("createBy", getCreateBy())
-            .append("createTime", getCreateTime())
-            .append("updateBy", getUpdateBy())
-            .append("updateTime", getUpdateTime())
-            .toString();
+        return new ToStringBuilder( this, ToStringStyle.MULTI_LINE_STYLE ).append( "id", getId() )
+                                                                          .append( "publickey", getPublickey() )
+                                                                          .append( "privatekey", getPrivatekey() )
+                                                                          .append( "urls", getUrls() )
+                                                                          .append( "secureUrls", getSecureUrls() )
+                                                                          .append( "createBy", getCreateBy() )
+                                                                          .append( "createTime", getCreateTime() )
+                                                                          .append( "updateBy", getUpdateBy() )
+                                                                          .append( "updateTime", getUpdateTime() ).toString();
     }
 }
