466941a4eec2d84fe31ec7babb8693ddd1aa2237
mengjun <mengjun8877@gmail.com>
2023-10-04 16:11:12 +0800

    1

--- game88-wallet/game88-wallet-api/src/main/java/tv/game88/wallet/api/service/impl/WalletUserServiceImpl.java
+++ game88-wallet/game88-wallet-api/src/main/java/tv/game88/wallet/api/service/impl/WalletUserServiceImpl.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tv.game88.common.utils.*;
 import tv.game88.common.vo.RspBase;
-import tv.game88.core.config.cache.ConfigDomainCacheUtil;
 import tv.game88.core.config.cache.ConfigEnvCacheUtil;
 import tv.game88.core.config.cache.GenerateOrderCacheUtils;
 import tv.game88.core.config.cache.SmsPhoneCacheUtil;
@@ -289,7 +288,7 @@ public class WalletUserServiceImpl extends ServiceImpl<WalletUserMapper, WalletU
         }
 
         Map<String, Object> resultMap = Maps.newHashMap();
-        resultMap.put( "url", ConfigDomainCacheUtil.me.getDomainOssValue() );
+        resultMap.put( "url", configEnvCacheUtil.getConf( "pay_host_url" ) );
         resultMap.put( "userInfo", this.baseMapper.selectPlatformUserByUserId( walletUser.getId() ) );
         resultMap.put( "walletAddress", walletUser.getId() );
         resultMap.put( "realName", walletUser.getRealName() );
