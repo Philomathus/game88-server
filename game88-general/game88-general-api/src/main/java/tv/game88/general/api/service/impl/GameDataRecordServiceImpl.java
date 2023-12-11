a69b2fafa096859cad97e3620d5a3ee93ea903c2
mengjun <mengjun8877@gmail.com>
2023-12-11 14:24:49 +0800

    1

--- game88-general/game88-general-api/src/main/java/tv/game88/general/api/service/impl/GameDataRecordServiceImpl.java
+++ game88-general/game88-general-api/src/main/java/tv/game88/general/api/service/impl/GameDataRecordServiceImpl.java
@@ -46,8 +46,7 @@ public class GameDataRecordServiceImpl extends ServiceImpl<GameDataRecordMapper,
         int                  i       = 0;
         int                  num     = 0;
         for ( GameDataRecord gameDataRecord : gameDataRecords ) {
-            LocalDateTime gameEndTime = LocalDateTimeUtils.parseLocalDateTime( gameDataRecord.getGameEndTime() );
-            String        day         = LocalDateTimeUtils.format( gameEndTime, LocalDateTimeUtils.YYYYMMDD_FORMATTER );
+            String        day         = LocalDateTimeUtils.format( LocalDateTime.now(), LocalDateTimeUtils.YYYYMMDD_FORMATTER );
             if ( mapper.findCount( gameDataRecord.getId(), TABLE_PREFIX + day ) > 0 ) {
                 continue;
             }
