81b93e8dece58b9676a5383f27d77d9806655415
mengjun <mengjun8877@gmail.com>
2023-12-30 21:57:03 +0800

    1

--- game88-core/game88-core-game/src/main/java/tv/game88/core/game/constants/ConstantsGame.java
+++ game88-core/game88-core-game/src/main/java/tv/game88/core/game/constants/ConstantsGame.java
@@ -28,7 +28,7 @@ public class ConstantsGame {
     public static final String PG_SOFT    = "PGSoft";
     public static final String SHABA      = "shaBa";
 
-    public static final String T1      = "T1";
+    public static final String T1      = "t1";
     public static final String PP      = "pp";
     public static final String CG      = "cg";
     public static final String JILI    = "jiLi";
