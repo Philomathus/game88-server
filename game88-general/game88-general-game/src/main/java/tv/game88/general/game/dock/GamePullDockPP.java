0c8132958d4f2c84c344d863e1474875be8f8afd
mengjun <mengjun8877@gmail.com>
2024-01-10 21:56:20 +0800

    1

--- game88-general/game88-general-game/src/main/java/tv/game88/general/game/dock/GamePullDockPP.java
+++ game88-general/game88-general-game/src/main/java/tv/game88/general/game/dock/GamePullDockPP.java
@@ -1,6 +1,9 @@
 package tv.game88.general.game.dock;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Repository;
@@ -14,11 +17,8 @@ import tv.game88.general.api.entity.GameDataRecord;
 import tv.game88.general.api.entity.GamePlatform;
 import tv.game88.general.game.base.AbstractGamePull;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 import java.util.List;
-import java.util.Map;
 
 @Log4j2
 @Repository( value = ConstantsGame.PP + "GamePullProcessor" )
@@ -50,20 +50,56 @@ public class GamePullDockPP extends AbstractGamePull {
         } );
         log.warn( resultStr );
         if ( StringUtils.isNotBlank( resultStr ) ) {
-            
+            //找到第一个换行符的位置
+            int index = resultStr.indexOf( "\n" );
+            //取出第一行数据
+            String firstLine = resultStr.substring( 0, index );
+            if ( StringUtils.isBlank( firstLine ) && !firstLine.contains( "=" ) ) {
+                return null;
+            } else {
+                String time = firstLine.split( "=" )[ 1 ];
+                gamePlatform.setVersionValue( time );
+            }
+            //删除第一行数据
+            resultStr = resultStr.substring( index + 1 );
+
+            try {
+                CSVParser csvParser = new CSVParser( new StringReader( resultStr ), CSVFormat.DEFAULT );
+                for ( CSVRecord csvRecord : csvParser ) {
+                    // 遍历每一行的字段
+                    for ( String field : csvRecord ) {
+                        System.out.print( field + " " );
+                    }
+                    System.out.println(); // 换行
+                }
+            } catch ( IOException e ) {
+                log.error( e.getMessage(), e );
+            }
         }
         return null;
     }
 
-    @Override
-    public GameDataRecord handleResult( Object object, GamePlatform gamePlatform ) {
-
-        return null;
+    public static void main( String[] args ) {
+        String str = """
+                timepoint=1704365926870
+                playerID,extPlayerID,gameID,playSessionID,parentSessionID,startDate,endDate,status,type,bet,win,currency,jackpot,bonusBet,bonusWin
+                """;
+        //找到第一个换行符的位置
+        int index = str.indexOf( "\n" );
+        //取出第一行数据
+        String firstLine = str.substring( 0, index );
+        if ( StringUtils.isBlank( firstLine ) && !firstLine.contains( "=" ) ) {
+        } else {
+            String time = firstLine.split( "=" )[ 1 ];
+            System.out.println( time );
+        }
+        //删除第一行数据
+        str = str.substring( index + 1 );
+        System.out.println( str );
     }
 
-    private String keyValStringFormat( final Map<String, Object> params ) {
-        StringBuilder sb = new StringBuilder();
-        params.forEach( ( k, v ) -> sb.append( k ).append( "=" ).append( v ).append( "&" ) );
-        return sb.substring( 0, sb.length() - 1 );
+    @Override
+    public GameDataRecord handleResult( Object object, GamePlatform gamePlatform ) {
+        return null;
     }
 }
