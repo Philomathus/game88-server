92b9707b2925ff1f3e0aa2a3bbf704b8154d6840
Robert <robertfeiwin@gmail.com>
2024-03-21 15:36:03 +0800

    [Add] create an api for wallet user paymethod in changing status

--- game88-wallet/game88-wallet-admin/src/main/java/tv/game88/wallet/admin/controller/WalletUserPayMethodController.java
+++ game88-wallet/game88-wallet-admin/src/main/java/tv/game88/wallet/admin/controller/WalletUserPayMethodController.java
@@ -2,14 +2,12 @@ package tv.game88.wallet.admin.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tv.game88.common.base.BaseController;
 import tv.game88.common.page.PageDomain;
 import tv.game88.common.page.TableSupport;
 import tv.game88.common.vo.RspBase;
+import tv.game88.core.admin.utils.SecurityUtils;
 import tv.game88.wallet.api.entity.WalletUserPayMethod;
 import tv.game88.wallet.api.service.WalletUserPayMethodService;
 import tv.game88.wallet.api.type.WalletPayMethodEnum;
@@ -42,4 +40,14 @@ public class WalletUserPayMethodController extends BaseController {
     public RspBase<List<String>> getPayMethodTypes() {
         return RspBase.ok( WalletPayMethodEnum.getPayMethodTypes() );
     }
+
+    @PutMapping( "/changeStatus/{methodId}" )
+    public RspBase<?> changeStatus( @PathVariable String methodId, Integer auditStatus, Integer googleAuthCode ) throws Exception {
+        SecurityUtils.verifyMFACode( googleAuthCode );
+        WalletUserPayMethod walletUserPayMethod = new WalletUserPayMethod();
+        walletUserPayMethod.setMethodId(Long.valueOf(methodId));
+        walletUserPayMethod.setAuditStatus( auditStatus );
+        return toResult( walletUserPayMethodService.updateById( walletUserPayMethod ) );
+    }
+
 }
