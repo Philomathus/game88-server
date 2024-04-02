4c4c9ecefb500225b3575b3271faef886dc5b389
rajesh <rkyadavsoft@gmail.com>
2024-04-02 15:08:11 +0800

    1

--- game88-wallet/game88-wallet-admin/src/main/java/tv/game88/wallet/admin/controller/WalletUserController.java
+++ game88-wallet/game88-wallet-admin/src/main/java/tv/game88/wallet/admin/controller/WalletUserController.java
@@ -71,7 +71,6 @@ public class WalletUserController extends BaseController {
         return RspBase.ok( walletUserService.getById( id ) );
     }
 
-
     @Log( title = "修改用户状态", businessType = BusinessType.UPDATE )
     @PutMapping( "/changeStatus/{memberId}" )
     public Object changeStatusBan( @PathVariable String memberId, Integer status, Integer googleAuthCode ) throws Exception {
