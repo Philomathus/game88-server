9b2aaf96b9e982af4a525c956c589a362fc5eced
mengjun <mengjun8877@gmail.com>
2024-04-12 22:38:02 +0800

    1

--- game88-game/game88-game-admin/src/main/java/tv/game88/game/admin/Game88GameAdminApplication.java
+++ game88-game/game88-game-admin/src/main/java/tv/game88/game/admin/Game88GameAdminApplication.java
@@ -2,12 +2,14 @@ package tv.game88.game.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication( scanBasePackages = { "tv.game88" } )
 @EnableScheduling
 @EnableAsync
+@EnableAspectJAutoProxy( exposeProxy = true )
 public class Game88GameAdminApplication {
     public static void main( String[] args ) {
         SpringApplication.run( Game88GameAdminApplication.class, args );
