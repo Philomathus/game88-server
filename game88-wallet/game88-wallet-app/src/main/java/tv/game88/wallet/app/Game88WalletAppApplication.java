06b8e9a6d5e920a5656c00d53f766e3ffb7b8ddf
rajesh <rkyadavsoft@gmail.com>
2024-04-16 10:38:36 +0800

    1

--- game88-wallet/game88-wallet-app/src/main/java/tv/game88/wallet/app/Game88WalletAppApplication.java
+++ game88-wallet/game88-wallet-app/src/main/java/tv/game88/wallet/app/Game88WalletAppApplication.java
@@ -3,6 +3,7 @@ package tv.game88.wallet.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAsync
 @EnableAspectJAutoProxy( exposeProxy = true )
+@EnableRetry
 public class Game88WalletAppApplication {
     public static void main( String[] args ) {
         SpringApplication.run( Game88WalletAppApplication.class, args );
--- game88-wallet/game88-wallet-app/src/main/java/tv/game88/wallet/app/listener/SubRedisMessageListener.java
+++ game88-wallet/game88-wallet-app/src/main/java/tv/game88/wallet/app/listener/SubRedisMessageListener.java
@@ -2,21 +2,27 @@ package tv.game88.wallet.app.listener;
 
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.RandomUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.stereotype.Component;
 import tv.game88.common.utils.StringUtils;
 import tv.game88.wallet.api.constants.ConstantsWallet;
 import tv.game88.wallet.api.type.StreamMessageType;
 import tv.game88.wallet.app.utils.SseSendMessageUtils;
 
-import java.time.Duration;
-
 /**
  * @author meng.jun
  */
 @Log4j2
+@Component
 public class SubRedisMessageListener implements MessageListener {
+    private final SseSendMessageUtils sseSendMessageUtils;
+
+    @Autowired
+    public SubRedisMessageListener( SseSendMessageUtils sseSendMessageUtils ) {
+        this.sseSendMessageUtils = sseSendMessageUtils;
+    }
 
     @Override
     public void onMessage( Message message, byte[] pattern ) {
@@ -32,30 +38,16 @@ public class SubRedisMessageListener implements MessageListener {
         String messageChannel = new String( message.getChannel() );
         if ( messageChannel.startsWith( ConstantsWallet.SSE_NOTIFICATION_CHANNEL ) ) {
             ConstantsWallet.MEMBER_SSEEMITTER_MAP
-                    .entrySet()
+                    .keySet()
                     .parallelStream()
-                    .forEach( ( entry ) -> SseSendMessageUtils.me.sendMessage( entry.getValue(), entry.getKey(), messageBody,
-                            StreamMessageType.NOTIFICATION ) );
+                    .forEach( ( key ) -> sseSendMessageUtils.sendMessage( key, messageBody, StreamMessageType.NOTIFICATION ) );
         }
         if ( messageChannel.startsWith( ConstantsWallet.SSE_MEMBER_CHANNEL ) ) {
             String memberId = messageChannel.replaceFirst( ConstantsWallet.SSE_MEMBER_CHANNEL, "" );
             if ( StringUtils.isBlank( memberId ) ) {
                 return;
             }
-            SseEmitter sseEmitter = ConstantsWallet.MEMBER_SSEEMITTER_MAP.get( memberId );
-            if ( sseEmitter == null ) {
-                try {
-                    Thread.sleep( Duration.ofSeconds( 3 ) );
-                } catch ( InterruptedException e ) {
-                    log.error( e.getMessage(), e );
-                }
-                sseEmitter = ConstantsWallet.MEMBER_SSEEMITTER_MAP.get( memberId );
-                if ( sseEmitter == null ) {
-                    log.warn( "会员{}不在线,无法发送个人消息", memberId );
-                    return;
-                }
-            }
-            SseSendMessageUtils.me.sendMessage( sseEmitter, memberId, messageBody, StreamMessageType.MEMBER );
+            sseSendMessageUtils.sendMessage( memberId, messageBody, StreamMessageType.MEMBER );
         }
     }
 }
