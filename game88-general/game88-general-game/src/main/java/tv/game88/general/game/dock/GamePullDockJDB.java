8f2f371d9adf68b510182d5f8fc2235e23fea533
mengjun <mengjun8877@gmail.com>
2024-05-02 15:39:03 +0800

    fix

--- game88-general/game88-general-game/src/main/java/tv/game88/general/game/dock/GamePullDockJDB.java
+++ game88-general/game88-general-game/src/main/java/tv/game88/general/game/dock/GamePullDockJDB.java
@@ -65,7 +65,8 @@ public class GamePullDockJDB extends AbstractGamePull {
 
         Map<String, Object> resultMap = this.sendPostMap( url, packageJson( requestMap ) );
 
-        //log.warn( JsonUtil.object2Json( resultMap ) );
+        log.warn( "url:{} - x:{} - request:{} - result:{}", url, json, JsonUtil.object2Json( requestMap ),
+                JsonUtil.object2Json( resultMap ) );
         if ( !CollectionUtils.isEmpty( resultMap ) ) {
             if ( "0000".equals( resultMap.getOrDefault( "status", "-1" ).toString() ) ) {
                 // 状态正常,无论是否有数据,从结束时间开始查询
