c36db80838328d33ef01c51fd6fde023fffcfaea
rajesh <rkyadavsoft@gmail.com>
2024-06-19 16:38:54 +0800

    1

--- game88-platform/game88-platform-api/src/main/java/tv/game88/platform/api/service/impl/MemberInfoServiceImpl.java
+++ game88-platform/game88-platform-api/src/main/java/tv/game88/platform/api/service/impl/MemberInfoServiceImpl.java
@@ -477,7 +477,7 @@ public class MemberInfoServiceImpl extends ServiceImpl<MemberInfoMapper, MemberI
             return RspBase.businessError( "请输入用户名" );
         }
         if ( mobileLogin.getUsername().length() < 6 || mobileLogin.getUsername().length() > 25 ) {
-            return RspBase.businessError( "用户名长度为8-25位" );
+            return RspBase.businessError( "用户名长度为6-25位" );
         }
         if ( StringUtils.isBlank( mobileLogin.getPasswd() ) ) {
             return RspBase.businessError( "请输入登陆密码" );
