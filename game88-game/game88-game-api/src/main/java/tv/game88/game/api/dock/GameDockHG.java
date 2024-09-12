d5080510e45dcf63f1ef54e0033cb6f261a71bf0
mengjun <mengjun8877@gmail.com>
2024-09-12 20:10:00 +0800

    1

--- game88-game/game88-game-api/src/main/java/tv/game88/game/api/dock/GameDockHG.java
+++ game88-game/game88-game-api/src/main/java/tv/game88/game/api/dock/GameDockHG.java
@@ -53,7 +53,7 @@ public class GameDockHG extends AbstractGameDock {
         params.put( "mode", MODE );
         String param = null;
         try {
-            log.warn( "createAccount params:{}", JsonUtil.object2Json( params ) );
+            log.warn( "createAccount params:{} md5:{}", JsonUtil.object2Json( params ), reqJoinGame.getMd5() );
             param = AESCoder.encryptDES3( JsonUtil.object2Json( params ), reqJoinGame.getMd5() );
         } catch ( Exception e ) {
             log.error( e.getMessage(), e );
