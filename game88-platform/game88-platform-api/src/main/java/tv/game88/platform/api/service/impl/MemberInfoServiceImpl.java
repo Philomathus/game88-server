a9458b8c9c284ba4ed26f37b9bdca253647f0e15
Rajesh <appsytech@gmail.com>
2024-12-08 10:10:58 +0800

    fix targetinstall 88

--- game88-platform/game88-platform-api/src/main/java/tv/game88/platform/api/service/impl/MemberInfoServiceImpl.java
+++ game88-platform/game88-platform-api/src/main/java/tv/game88/platform/api/service/impl/MemberInfoServiceImpl.java
@@ -309,7 +309,7 @@ public class MemberInfoServiceImpl extends ServiceImpl<MemberInfoMapper, MemberI
                 this.baseMapper.deleteByHistoryKey( oldm.getId() );
             } else {
                 //渠道邀请码注册通知(归档会员回归不通知)
-                regChannelNotice( mobileLogin, dev, memberInfo.getId() );
+                regChannelNotice( mobileLogin, dev,version, memberInfo.getId() );
             }
         }
         RspMember rspMember = new RspMember();
@@ -413,10 +413,10 @@ public class MemberInfoServiceImpl extends ServiceImpl<MemberInfoMapper, MemberI
             this.baseMapper.insert( memberInfo );
             try {
                 //渠道邀请码注册通知(归档会员回归不通知)
-                regChannelNotice( mobileLogin, dev, memberInfo.getId() );
+                regChannelNotice( mobileLogin, dev,version, memberInfo.getId() );
             } catch ( Exception e ) {
                 try {
-                    regChannelNotice( mobileLogin, dev, memberInfo.getId() );
+                    regChannelNotice( mobileLogin, dev,version, memberInfo.getId() );
                 } catch ( Exception p ) {
                     log.error( "反作弊注册成功，通知推广渠道失败 account:{},errMsg:{}", memberInfo.getId(), p.getMessage() );
                 }
@@ -471,10 +471,10 @@ public class MemberInfoServiceImpl extends ServiceImpl<MemberInfoMapper, MemberI
         this.baseMapper.insert( memberInfo );
         try {
             //渠道邀请码注册通知(归档会员回归不通知)
-            regChannelNotice( mobileLogin, dev, memberInfo.getId() );
+            regChannelNotice( mobileLogin, dev,version, memberInfo.getId() );
         } catch ( Exception e ) {
             try {
-                regChannelNotice( mobileLogin, dev, memberInfo.getId() );
+                regChannelNotice( mobileLogin, dev,version, memberInfo.getId() );
             } catch ( Exception p ) {
                 log.error( "通知推广渠道失败 account:{},errMsg:{}", memberInfo.getId(), p.getMessage() );
             }
@@ -647,7 +647,12 @@ public class MemberInfoServiceImpl extends ServiceImpl<MemberInfoMapper, MemberI
     }
 
 
-    private void regChannelNotice( MobileLogin mobileLogin, Integer dev, String userId ) {
+    private void regChannelNotice( MobileLogin mobileLogin, Integer dev,String version, String userId ) {
+
+        if( AppVersionUtils.hasNewVersion("6.12.3.0", version ) ) {
+            return;
+        }
+
         String noticeUrl = configEnvCacheUtil.getConf( "channel_reg_notice" );
         if ( StringUtils.isBlank( noticeUrl ) ) {
             log.error( "平台无法找到环境变量channel_reg_notice,userId:{}", userId );
